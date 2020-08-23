71adbd97bb8f49d4e5aaa79f8bbbf9ba22fb3caa
williamChang80 <william_chang80@rocketmail.com>
2020-08-24 00:46:51 +0700

    Add env and serve

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/williamchang80/sea-apd
 
 go 1.13
+
+require github.com/joho/godotenv v1.3.0 // indirect
--- main.go
+++ main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Cannot read .env file")
+	}
+	appPort := ":" + os.Getenv("APP_PORT")
+	appHost := fmt.Sprintf("http://%s%v", os.Getenv("APP_HOST"), appPort)
+	fmt.Println("App is running on " + appHost)
+	http.ListenAndServe(appPort, nil)
+}
